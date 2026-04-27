70d10a81305a689e7b8af4ba1f63c98141279efc
Jarod <your-email@gmail.com>
2026-04-27 12:03:04 +1000

    Set homepage as root page

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "home.html"));
 });
 
 mongoose
